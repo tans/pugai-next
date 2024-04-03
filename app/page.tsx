db37c8e7ec3b9a5922409643446f49054e8b091b
fun <jun@mankei.cc>
2024-04-03 10:49:53 +0800

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,124 @@
 import Image from "next/image";
+import { BotIcon, UserIcon } from "lucide-react";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="mx-auto max-w-2xl p-4 leading-8">
+      <div className="my-4 flex items-center text-lg font-black">
+        <img src="logo.webp" className="w-16 rounded-full" />
+        <div className="mx-2">微信表格机器人</div>
+      </div>
+      <div className="my-2 rounded bg-blue-50 p-2 text-blue-900">
+        三步让微信机器人成为微信里的Excel工具
+      </div>
+
+      <div>
+        <div className="rounded-md rounded-sm bg-gray-50 p-2 leading-6 text-gray-700 text-gray-800">
+          <div className="text-center font-black">李鲁味集团销售内部群</div>
+          <div className="flex  items-start justify-end ">
+            <div className="bg-green-50 p-2">
+              <div>创建表格:春暖花开</div>
+              <div>关键字:促销活动</div>
+              <div>字段:战队</div>
+              <div>字段:成员</div>
+              <div>字段:行动</div>
+              <div>字段:积分</div>
+            </div>
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <UserIcon className="mx-auto"></UserIcon>
+              <div className="text-xs text-gray-400">管理员</div>
+            </div>
+          </div>
+
+          <div className="flex items-start justify-start  ">
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <BotIcon className="mx-auto"></BotIcon>
+              <div className="text-xs text-gray-400">机器人</div>
+            </div>
+            <div className="bg-green-50 p-2">创建表格成功</div>
+          </div>
+
+          <div className="flex items-start justify-end">
+            <div className="bg-green-50 p-2">
+              <div>促销活动:春暖花开</div>
+              <div>战队:A战队</div>
+              <div>成员:老田</div>
+              <div>行动:引流到店</div>
+              <div>积分:10</div>
+            </div>
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <UserIcon className="mx-auto"></UserIcon>
+              <div className="text-xs text-gray-400">业务员</div>
+            </div>
+          </div>
+
+          <div className="flex items-start justify-start">
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <BotIcon className="mx-auto"></BotIcon>
+              <div className="text-xs text-gray-400">机器人</div>
+            </div>
+            <div className="bg-green-50 p-2">插入数据成功</div>
+          </div>
+
+          <div className="flex items-start justify-end">
+            <div className="bg-green-50 p-2">
+              <p>导出表格:春暖花开</p>
+            </div>
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <UserIcon className="mx-auto"></UserIcon>
+              <div className="text-xs text-gray-400">管理员</div>
+            </div>
+          </div>
+
+          <div className="flex items-start justify-start">
+            <div className="mx-1 rounded-lg border bg-white p-2 text-center">
+              <BotIcon className="mx-auto"></BotIcon>
+              <div className="text-xs text-gray-400">机器人</div>
+            </div>
+            <div className="bg-green-50 p-2">导出成功 *****.csv</div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Table className="my-4 rounded-sm border">
+        <TableCaption>导出Excel进行数据处理</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">战队</TableHead>
+            <TableHead>成员</TableHead>
+            <TableHead>行动</TableHead>
+            <TableHead className="text-right">积分</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">A战队</TableCell>
+            <TableCell>老田</TableCell>
+            <TableCell>引流到店</TableCell>
+            <TableCell className="text-right">10</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <div className="mt-8">
+        <div>免费内测开放中，支持源码定制部署</div>
+        <div>添加微信 tianshe00</div>
+        <img src="qrcode.webp" className="w-96" />
+        {/* <div>
+          <div>更多模板</div>
+          <div></div>
+        </div> */}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
